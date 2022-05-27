5335ae3e0c498bdd882711fff82461425d800877
Semih Esentürk <semihesenturk@outlook.com>
2022-05-27 13:45:31 +0300

    Domain Entites has added
    Domain Entites has added  for Contact microservice

---
+++ src/Contact/Api/Core/Contact.Domain/Contact.Domain/Models/Contact.cs
@@ -0,0 +1,12 @@
+﻿namespace Contact.Domain.Models;
+
+public class Contact : BaseEntity
+{
+    public string PhoneNumber { get; set; }
+    public string EmailAddress { get; set; }
+    public string Location { get; set; }
+    public Guid PersonId { get; set; }
+
+    public Person Person { get; set; }
+
+}
---
+++ src/Contact/Api/Core/Contact.Domain/Contact.Domain/Models/Person.cs
@@ -0,0 +1,10 @@
+﻿namespace Contact.Domain.Models;
+
+public class Person : BaseEntity
+{
+    public string Name { get; set; }
+    public string Surname { get; set; }
+    public string Company { get; set; }
+
+    public virtual ICollection<Contact> Contacts { get; set; }
+}
