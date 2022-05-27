a4d7759411aa5da3b118dc18c592e0567ef7ee9b
Semih Esentürk <semihesenturk@outlook.com>
2022-05-27 13:57:28 +0300

    DI operations for Contact Application layer
    DI operations for Contact Application layer

---
+++ src/Contact/Api/Core/Contact.Application/Contact.Application/Extensions/Registration.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace Contact.Application.Extensions;
+
+public static class Registration
+{
+    public static IServiceCollection AddApplicationRegistration(this IServiceCollection services)
+    {
+        var assm = Assembly.GetExecutingAssembly();
+
+        services.AddMediatR(assm);
+        services.AddAutoMapper(assm);
+        services.AddValidatorsFromAssembly(assm);
+
+        return services;
+    }
+}
---
+++ src/Contact/Api/Core/Contact.Application/Contact.Application/Interfaces/IPersonRepository.cs
@@ -0,0 +1,8 @@
+﻿using Contact.Application.Interfaces.Repositories;
+using Contact.Domain.Models;
+
+namespace Contact.Application.Interfaces;
+
+public interface IPersonRepository : IGenericRepository<Person>
+{
+}
